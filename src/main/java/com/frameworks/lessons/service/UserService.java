e3dab476fe3b6076645953b7aa185c2e4393be07
alx3 <40515250+alx3@users.noreply.github.com>
2018-07-20 22:08:14 +0300

    Update UserService.java

--- src/main/java/com/frameworks/lessons/service/UserService.java
+++ src/main/java/com/frameworks/lessons/service/UserService.java
@@ -1,10 +1,15 @@
 package com.frameworks.lessons.service;
 
 import com.frameworks.lessons.entity.User;
-
 import java.util.List;
 
 public interface UserService {
+	User findByName(String name);
+	User findById(int id);
+	
     void add(User user);
+    void update(User user);
+    void delete(User user);
+    
     List<User> listUsers();
 }
