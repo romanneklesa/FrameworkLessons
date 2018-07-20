72f05bc50c4e8b4a83f94d863681cc3718ee455c
alx3 <40515250+alx3@users.noreply.github.com>
2018-07-20 22:39:11 +0300

    Create AccountService.java

---
+++ src/main/java/com/frameworks/lessons/service/AccountService.java
@@ -0,0 +1,17 @@
+package com.frameworks.lessons.service;
+
+import java.util.List;
+
+import com.frameworks.lessons.entity.Account;
+
+public interface AccountService {
+	
+	Account getById(int id);
+
+	void add(Account account);
+	void update(Account account);
+	void delete(Account account);
+
+	List<Account> listAccounts();
+	
+}
