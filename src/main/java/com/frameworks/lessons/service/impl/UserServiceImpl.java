7143ffb2390082d7f425655d11013746ee359de0
alx3 <40515250+alx3@users.noreply.github.com>
2018-07-20 22:22:00 +0300

    Update UserServiceImpl.java

--- src/main/java/com/frameworks/lessons/service/impl/UserServiceImpl.java
+++ src/main/java/com/frameworks/lessons/service/impl/UserServiceImpl.java
@@ -1,1 +1,56 @@
+package com.frameworks.lessons.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.frameworks.lessons.dao.UserDao;
+import com.frameworks.lessons.entity.User;
+import com.frameworks.lessons.service.UserService;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserDao dao;
+
+	@Override
+	@Transactional(readOnly=true)
+	public User findByName(String name) {
+		return dao.findByName(name);
+	}
+
+	@Override
+	@Transactional(readOnly=true)
+	public User findById(int id) {
+		return dao.getUser(id);
+	}
+
+	@Override
+	@Transactional
+	public void add(User user) {
+		dao.add(user);
+	}
+
+	@Override
+	@Transactional
+	public void update(User user) {
+		dao.updateUser(user);
+	}
+
+	@Override
+	@Transactional
+	public void delete(User user) {
+		dao.deleteUser(user.getId());
+	}
+
+	@Override
+	@Transactional(readOnly=true)
+	public List<User> listUsers() {
+		return dao.listUsers();
+	}
+}
+
 
