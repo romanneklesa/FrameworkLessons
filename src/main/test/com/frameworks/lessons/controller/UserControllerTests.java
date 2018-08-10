8ee8b4c66071b02c3d93eb2541fa253b21eee5ce
Roman Neklesa <neklesar@gmail.com>
2018-08-10 16:02:30 +0300

    some tests

--- src/main/test/com/frameworks/lessons/controller/UserControllerTests.java
+++ src/main/test/com/frameworks/lessons/controller/UserControllerTests.java
@@ -1,4 +1,69 @@
 package com.frameworks.lessons.controller;
 
+import com.frameworks.lessons.entity.Account;
+import com.frameworks.lessons.entity.User;
+import com.frameworks.lessons.model.Role;
+import com.frameworks.lessons.service.AccountService;
+import com.frameworks.lessons.service.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import java.util.List;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@ComponentScan("com.frameworks.lessons")
 public class UserControllerTests {
+
+    @InjectMocks
+    private UserController userController;
+
+    @Mock
+    private AccountService accountService;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.standaloneSetup(userController, accountService)
+                .setViewResolvers(resolver).build();
+    }
+
+    @Test
+    public void testUserPage() throws Exception {
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/user"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("user"))
+                .andDo(print())
+                .andReturn();
+    }
+
+    @Test
+    public void testCurrentuseraccounts() throws Exception {
+
+
+    }
+
+    @Test
+    public void testUpdateAmount(){
+
+    }
 }
--- src/main/test/com/frameworks/lessons/dao/UserDaoImplTests.java
+++ src/main/test/com/frameworks/lessons/dao/UserDaoImplTests.java
@@ -1,4 +1,69 @@
 package com.frameworks.lessons.dao;
 
+import com.frameworks.lessons.config.AppConfig;
+import com.frameworks.lessons.config.WebSecurityConfig;
+import com.frameworks.lessons.entity.User;
+import com.frameworks.lessons.model.Role;
+import org.hibernate.SessionFactory;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {WebSecurityConfig.class, AppConfig.class})
+
 public class UserDaoImplTests {
+
+    @Mock
+    private UserDao userDao;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testGetUser(){
+        User mockedUser = new User();
+
+        mockedUser.setId(1);
+        mockedUser.setName("admin");
+
+        when(userDao.getUser(1)).thenReturn(mockedUser);
+        User testName = userDao.getUser(1);
+        Assert.assertEquals(mockedUser, testName);
+
+    }
+
+    @Test
+    public void testSave(){
+        userDao.add(getSampleUser());
+
+    }
+
+    @Test
+    public void findAllEmployees(){
+        Assert.assertEquals(userDao.listUsers().size(),anyInt());
+    }
+
+
+
+    public User getSampleUser(){
+        User user = new User();
+        user.setName("arminvb");
+        user.setId(34);
+        user.setPassword("qweee");
+        user.setEmail("armin@i.ua");
+        user.setRole(Role.USER);
+        return user;
+    }
+
 }
